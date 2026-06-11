69989583de8f5eae2c34c725c002e083b7ba204d
Shivaji Kishanrao Suryawanshi <dainikkishanprabhat@gmail.com>
2026-06-11 15:52:15 +0530

    Updated live website message

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('DevOps Pipeline & DNS Configuration Successfully Completed! 🚀');
-});
-
+app.get('/', (req, res) => res.send('Wow! My Automated DevOps Project is Live on AWS! 🚀'));
 app.listen(3000, () => console.log('Server running on port 3000'));
