97c603e5319dd92c2278d6bc7ddb0d37357f18bd
Shivaji Kishanrao Suryawanshi <dainikkishanprabhat@gmail.com>
2026-06-11 07:12:33 +0530

    Fixed server syntax error

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('res.send('DevOps Pipeline & DNS Configuration Successfully Completed! 🚀');
-');
+
+app.get('/', (req, res) => {
+    res.send('DevOps Pipeline & DNS Configuration Successfully Completed! 🚀');
+});
+
 app.listen(3000, () => console.log('Server running on port 3000'));
